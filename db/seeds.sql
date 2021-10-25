14fba96d6dba82fb22d55faf7149dc33e8f2e923
Umera <umeramalek796@gmail.com>
2021-10-25 02:56:35 -0700

    started adding values to the tables

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (dep_name)
+VALUES  ('Engineering'),
+        ('Accounting'),
+        ('Legal'),
+        ('Sales'),
+        ('Service');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('CEO')
+        ('Manager')
+        ('Auditor')
+        ('Accountant')
+        ('Lawyer')
+        ('Sales Rep')
+        ('Customer Service')
+        ('Engineer');
+
+
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES 
